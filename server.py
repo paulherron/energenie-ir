0f9e6377c39e17ed14f86ebecd55cf7dfb28509c
Paul Herron <contact@paulherron.net>
2017-01-07 14:45:26 +0000

    Initial commit.

---
+++ server.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify, Response
+from flask_cors import CORS, cross_origin
+import datetime
+import json
+import os
+import sys
+app = Flask(__name__)
+CORS(app)
+
+status_file_path = os.path.dirname(__file__) + '/status.json'
+
+@app.route('/')
+def status():
+    return get_status()
+
+@app.route('/down')
+def down():
+    os.system('irsend SEND_ONCE velux KEY_DOWN --count=10')
+    update_status('down')
+    return get_status()
+
+@app.route('/up')
+def up():
+    os.system('irsend SEND_ONCE velux KEY_UP --count=10')
+    update_status('up')
+    return get_status()
+
+def update_status(status):
+    with open(status_file_path, 'w') as status_file:
+        json.dump({'lastCommand': status, 'lastCommandTime': datetime.datetime.now().isoformat()}, status_file)
+
+def get_status():
+    with open(status_file_path, 'r') as status_file:
+        return jsonify(status_file.read())
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
